01f41a84fadc96348983d044f6028d6a50f294df
aa667248 <89883809+aa667248@users.noreply.github.com>
2021-09-30 22:30:12 +0900

    Update cursor.js

--- assets/js/cursor.js
+++ assets/js/cursor.js
@@ -6,8 +6,8 @@ $(function(){
     //mousemoveイベントでカーソル要素を移動させる
     $(document).on("mousemove",function(e){
       //カーソルの座標位置を取得
-      var x=e.clientX;
-      var y=e.clientY;
+      x=e.clientX;
+      y=e.clientY;
       //カーソル要素のcssを書き換える用
       cursor.css({
         "opacity":"1",
