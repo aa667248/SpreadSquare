0b31374a6a4301ab796f9a872cddc43780e87ff3
aa667248 <89883809+aa667248@users.noreply.github.com>
2021-09-30 12:59:03 +0900

    Delete judge.js

--- judge.js
+++
@@ -1,9 +0,0 @@
-$(document).on('mouseenter','div',function(){
-    if(stopgame==false){
-	document.getElementById('cursor').style.background = 'red';
-    document.getElementById('score').style.display = 'inline';
-    stopgame=true
-    phase=phase-1
-    document.getElementById('marker').insertAdjacentHTML('afterbegin','<p>これでリスタート<br>※広がる四角が消えるまで待たないとバグる<br>↓↓↓↓</P><input type="button" onclick="restart()">')
-    }
-});
